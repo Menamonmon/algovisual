ffeb37dd62770c62aac32c656cc2c142be6472f4
Menamonmon <monyasser111@gmail.com>
2021-03-27 02:04:17 -0400

    created navbar + navitem

---
+++ components/NavItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+import { NavItem as Props } from "../types";
+
+const NavItem: React.FC<Props> = ({ name, path }) => (
+  <Link href={path} key={`${name}-${path}`}>
+    <p className="nav-btn capitalize mx-2 p-1 self-center">{name}</p>
+  </Link>
+);
+
+export default NavItem;
---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar: React.FC = ({ children }) => (
+  <nav className="bg-blue-600 w-screen flex justify-between">
+    <div>
+      <Link href="/">
+        <h1 className="nav-btn text-3xl m-5 p-2">AlgoVisual</h1>
+      </Link>
+    </div>
+    <ul className="flex items-stretch ">{children}</ul>
+  </nav>
+);
+
+export default Navbar;
