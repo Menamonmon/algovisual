569f8cc8c9a1c9c1d230e2a8838b4daac5636b27
Menamonmon <monyasser111@gmail.com>
2021-03-28 12:33:21 -0400

    fixed bug with board generation

--- utils/generateBoard.tsx
+++ utils/generateBoard.tsx
@@ -4,13 +4,14 @@ const generateBoard = (
   w: number,
   h: number,
   defaultBoardCell: BoardCell
-): Array<Array<BoardCell>> => {
-  const board: Array<Array<BoardCell>> = [];
+): BoardCell[][] => {
+  const board: BoardCell[][] = [];
   for (let y = 0; y < h; y++) {
     let row: BoardCell[] = [];
     for (let x = 0; x < w; x++) {
       row.push(defaultBoardCell);
     }
+    board.push(row);
   }
   return board;
 };
