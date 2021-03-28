3b24cb302c84448c3eac1daf248c62e57539d200
Menamonmon <monyasser111@gmail.com>
2021-03-28 12:21:26 -0400

    changed name of pos validation function

--- utils/isPositionValid.tsx
+++ utils/isPositionValid.tsx
@@ -1,8 +1,10 @@
 import { Position } from "../types";
 
-const validatePosition = (pos: Position, w: number, h: number): boolean => {
+const isPositionValid = (pos: Position, w: number, h: number): boolean => {
   if (!(pos.x >= 0 && pos.x < w && pos.y >= 0 && pos.y < h)) {
     return false;
   }
   return true;
 };
+
+export default isPositionValid;
