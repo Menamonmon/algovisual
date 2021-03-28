34bcdac1b4e144ebeb9d516309cd6febc97ab181
Menamonmon <monyasser111@gmail.com>
2021-03-28 11:29:20 -0400

    a folder that will have the information for each algorithm

---
+++ algoinfo/pathfinding.tsx
@@ -0,0 +1,12 @@
+import { AlgoInfoMap } from "../types";
+
+const pathfindingInfo: AlgoInfoMap = new Map();
+pathfindingInfo.set("dfs", {
+  fullName: "Depth-First Search",
+  wikiLink: "https://wikipedia.com",
+});
+pathfindingInfo.set("bfs", {
+  fullName: "Breadth-First Search",
+  wikiLink: "https://wikipedia.com",
+});
+export default pathfindingInfo;
