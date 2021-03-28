cc256dbfeb339e59b090cd7d5dfd6041e3fb4830
Menamonmon <monyasser111@gmail.com>
2021-03-27 22:47:38 -0400

    homepage now uses layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,16 @@
+import React from "react";
 import Head from "next/head";
+import Layout from "../components/Layout";
 
-import Navbar from "../components/Navbar";
-import NavItem from "../components/NavItem";
-import DropDownMenu from "../components/DropDownMenu";
-
-import Footer from "../components/Footer";
-import FooterItem from "../components/FooterItem";
-import FooterIcon from "../components/FooterIcon";
-
-import navLinks from "../links/nav";
-import footerLinks from "../links/footer";
-import socialMediaLinks from "../links/social";
-
-export default function Home() {
-  return (
-    <div className="container">
+const HomePage = () => (
+  <Layout
+    head={
       <Head>
-        <title>Home | AlgoVisual</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Home | Algovisual</title>
       </Head>
-      <Navbar>
-        {navLinks.dropdowns?.map((dropdown, idx) => (
-          <DropDownMenu {...dropdown} key={idx} />
-        ))}
-        {navLinks.navitems?.map((navitem, idx) => (
-          <NavItem {...navitem} key={idx + 10000} />
-        ))}
-      </Navbar>
-      <Footer>
-        {footerLinks.map((link, idx) => (
-          <FooterItem {...link} key={idx} />
-        ))}
-        {socialMediaLinks.map((link, idx) => (
-          <FooterIcon {...link} key={idx + 10000} />
-        ))}
-      </Footer>
-    </div>
-  );
-}
+    }
+  >
+  </Layout>
+);
+
+export default HomePage;
