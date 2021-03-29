e2b61d744a6c23ecf64e8d2684c959117dfde884
Menamonmon <monyasser111@gmail.com>
2021-03-29 11:42:48 -0400

    added cell color code value

--- components/algorithms/Pathfinding/Board.tsx
+++ components/algorithms/Pathfinding/Board.tsx
@@ -3,6 +3,7 @@ import useContinuousClickListener from "../../../hooks/useContinuousClickListene
 import usePathfindingBoard from "../../../hooks/usePathfindingBoard";
 import { PathfindingBoardCell, Position } from "../../../types";
 import Cell from "./Cell";
+import CellColorCode from "./CellColorCode";
 
 type BoardType = PathfindingBoardCell[][];
 
@@ -12,10 +13,7 @@ interface Props {
 }
 
 const Board: React.FC<Props> = ({ w, h }) => {
-  const {
-    board,
-    updateBoard,
-  } = usePathfindingBoard(w, h);
+  const { board, updateBoard } = usePathfindingBoard(w, h);
   const boardRef = useRef<HTMLDivElement>(null);
   const [isMouseDown, setIsMouseDown] = useState(false);
 
@@ -23,37 +21,48 @@ const Board: React.FC<Props> = ({ w, h }) => {
   w = board[0].length;
   const boardPxlWidth = 500;
   const boardPxlHeight = 500;
-  const boardCells = board.map((row, y) =>
-    row.map((c, x) => (
-      <Cell
-        cell={c}
-        pos={{ x, y }}
-        key={`${x}-${y}-${c}`}
-        w={boardPxlWidth / w}
-        onMouseEnter={() => {
-          if (isMouseDown) {
-            updateBoard({x, y}, c === "uv" ? "w" : "uv")
-          }
-        }}
-      />
-    ))
-  );
+  const cellWidth = boardPxlWidth / w;
+  const cellNamesMap = new Map<PathfindingBoardCell, string>();
+  cellNamesMap.set("uv", "Unvisitied");
+  cellNamesMap.set("v", "Visited");
+  cellNamesMap.set("w", "Wall");
+  cellNamesMap.set("s", "Start");
+  cellNamesMap.set("e", "End");
 
   return (
-    <div
-      className="my-5 mx-auto bg-white"
-      style={{
-        display: "grid",
-        width: boardPxlWidth,
-        height: boardPxlHeight,
-        gridTemplateColumns: `repeat(${w}, 1fr)`,
-        gridTemplateRows: `repeat(${h}, 1fr)`,
-      }}
-      ref={boardRef}
-      onMouseDown={() => {setIsMouseDown(true)}}
-      onMouseUp={() => setIsMouseDown(false)}
-    >
-      {boardCells}
+    <div>
+      <CellColorCode cellWidth={cellWidth} cellNamesMap={cellNamesMap} />
+      <div
+        className="my-5 mx-auto bg-white"
+        style={{
+          display: "grid",
+          width: boardPxlWidth,
+          height: boardPxlHeight,
+          gridTemplateColumns: `repeat(${w}, 1fr)`,
+          gridTemplateRows: `repeat(${h}, 1fr)`,
+        }}
+        ref={boardRef}
+        onMouseDown={() => {
+          setIsMouseDown(true);
+        }}
+        onMouseUp={() => setIsMouseDown(false)}
+      >
+        {board.map((row, y) =>
+          row.map((c, x) => (
+            <Cell
+              cell={c}
+              pos={{ x, y }}
+              key={`${x}-${y}-${c}`}
+              w={cellWidth}
+              onMouseEnter={() => {
+                if (isMouseDown) {
+                  updateBoard({ x, y }, c === "uv" ? "w" : "uv");
+                }
+              }}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
